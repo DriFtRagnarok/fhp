00d78d00230acdb8e1d13a6d592b501daf6ae6fb
Logan <49781823+DriFtRagnarok@users.noreply.github.com>
2019-07-17 20:28:36 -0500

    Add files via upload

---
+++ SASP-PACK-MRP/Tahoe/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files{
+    'data/carcols.ymt',
+    'data/carvariations.ymt',
+}
+
+data_file 'CARCOLS_FILE' 'data/carcols.ymt'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.ymt'
